feeb2807a8c0c3279bbd8098270e796e02ec7da5
Maycon Medeiros <maycon1038@gmail.com>
2020-02-04 10:28:08 -0400

    teste de package

---
+++ mytestpackgerithub/src/main/java/br/com/msm/mytestpackgegithub/App.java
@@ -0,0 +1,13 @@
+package br.com.msm.mytestpackgegithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
